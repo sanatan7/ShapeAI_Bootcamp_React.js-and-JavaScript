b9d36d9cde890ca6033ffe2a873699f044703221
sanatan7 <85389088+sanatan7@users.noreply.github.com>
2021-06-05 14:59:02 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Header(){
+  return(
+    <div>
+      <header><h1>ShapeAI</h1></header>
+    </div>
+  )
+};
---
+++ Notes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Notes(){
+  return(
+    <div className = "note">
+      <h1>Javascript and React.js</h1>
+      <p>This was an amazing bootcamp taken up by Shaurya Sinha.
+        We covered everything fom scratch including Javasccript, React.js, HTML.
+      </p>
+    </div>
+  )
+}
